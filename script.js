9faaf4e83002ef003761454e19100c441a9dab0d
Chelsea <186109435+seaarmadillo@users.noreply.github.com>
2025-02-16 12:00:21 -0600

    create html, css, js files set html structure create div with flexbox in js file

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const grid = document.createElement("div");
+grid.classList.add("grid");
+grid.textContent = "this will have a grid";
+
+grid.style.display = "flex";
+grid.style.justifyContent = "center";
+grid.style.alignItems = "center";
+
+// Append the grid element to the body
+document.body.appendChild(grid);
+
+
+
