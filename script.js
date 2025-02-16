5fd2acc8c00ccbb5d87bf2fb258aa022309fc9f1
Chelsea <186109435+seaarmadillo@users.noreply.github.com>
2025-02-16 12:09:37 -0600

    add flex styles to the container, include height, width, background color,

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-
+// Create a div element with flexbox properties
 const grid = document.createElement("div");
 grid.classList.add("grid");
 grid.textContent = "this will have a grid";
 
 grid.style.display = "flex";
+grid.style.flexWrap = "wrap";
 grid.style.justifyContent = "center";
 grid.style.alignItems = "center";
+
+grid.style.backgroundColor = "#f9cbdd";
+grid.style.border = "1px #d18ca6 solid";
+grid.style.width = "100%";
+grid.style.height = "200px";
+
+
 
 // Append the grid element to the body
 document.body.appendChild(grid);
