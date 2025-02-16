125a2bd600271ad4316da9abf053dfd5786aff80
Chelsea <186109435+seaarmadillo@users.noreply.github.com>
2025-02-16 12:22:18 -0600

    add loop to create window filled with 16px x 16px divs

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 // Create a div element with flexbox properties
 const grid = document.createElement("div");
 grid.classList.add("grid");
-grid.textContent = "this will have a grid";
 
 grid.style.display = "flex";
 grid.style.flexWrap = "wrap";
 grid.style.justifyContent = "center";
 grid.style.alignItems = "center";
 
-grid.style.backgroundColor = "#f9cbdd";
-grid.style.border = "1px #d18ca6 solid";
-grid.style.width = "100%";
-grid.style.height = "200px";
+
+grid.style.width = "100vw";
+grid.style.height = "100vh";
 
 
+// Create div elements with flexbox properties
 
+for (let i = 0; i < (window.innerWidth / 16) * (window.innerHeight / 16); i++) { 
+    const item = document.createElement("div");
+ 
+   
+    item.style.border = ".5px solid #000";
+    item.style.boxSizing = "border-box";
+    item.style.margin = "0";
+    item.style.textAlign = "center";
+    item.style.width = "16px"; 
+    item.style.height = "16px"; 
+    grid.appendChild(item);
+
+}
 // Append the grid element to the body
-document.body.appendChild(grid);
-
-
-
+document.body.appendChild(grid); 
